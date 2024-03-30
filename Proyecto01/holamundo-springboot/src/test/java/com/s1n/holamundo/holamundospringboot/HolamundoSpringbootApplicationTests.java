bb0badec6f7bce99a20b9700d4e631d7e5aa4cd2
jpabl@0s1n <jpablo_166@hotmail.com>
2024-03-30 15:40:38 -0500

    feat:first proyect Hello World

---
+++ Proyecto01/holamundo-springboot/src/test/java/com/s1n/holamundo/holamundospringboot/HolamundoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1n.holamundo.holamundospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
